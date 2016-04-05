4753f2a01871f65bf47d683b8a448bedae45a90d
HavenShen <havenshen@gmail.com>
2016-04-05 15:28:06 +0800

    一个web hook初始化

--- main.py
+++ main.py
@@ -5,7 +5,8 @@
 # Created By HavenShen on 2013-11-29,Version 0.1
 
 import comm_log
-import time
+import subprocess
+import json
 import tornado.ioloop
 import tornado.web
 import tornado.options
@@ -16,12 +17,21 @@ define("port", default=8765, help="run on the given port", type=int)
 #日志输出
 define("log", default=comm_log.get_logging('gohook'), help="TOKEN", type=str)
 
+file_path = '/Users/HavenShen/Desktop/gogit'
+
+def pull():
+    cmd = ['cd', file_path, '&&','git','pull']
+    p = subprocess.Popen(cmd,cwd=file_path)
+    p.wait()
 
 class MainHandler(tornado.web.RequestHandler):
+
+
     #get请求，验证微信接口
-    def get(self):
+    def get(self,response):
+        json = tornado.escape.json_decode(response.body)
         #输出log
-        options.log.info('loginfo:done')
+        options.log.info('loginfo:done' + json)
         #验证成功返回
         self.write('done')
 
@@ -38,5 +48,4 @@ application = tornado.web.Application([
 
 if __name__ == "__main__":
     application.listen(options.port)
-    print 'ok'
     tornado.ioloop.IOLoop.instance().start()
